834676dbab7270b46b9c960b7d762704cf6ef0dd
andrewemmons95 <60409142+andrewemmons95@users.noreply.github.com>
2020-03-08 23:22:38 -1000

    Section 4

--- app/client/main.js
+++ app/client/main.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 
+import '../imports/api/tasks.js';
+
 import App from '../imports/ui/App.js';
 
-import '../imports/api/tasks.js';
+
 
 Meteor.startup(() => {
   render(<App />, document.getElementById('render-target'));
--- app/imports/ui/App.js
+++ app/imports/ui/App.js
@@ -1,22 +1,43 @@
 import React, { Component } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
+import ReactDOM from 'react-dom';
 
 import { Tasks } from '../api/tasks.js';
 
 import Task from './Task.js';
 
   class App extends Component {
-  renderTasks() {
-    return this.props.tasks.map((task) => (
-        <Task key={task._id} task={task} />
-    ));
-  }
+    handleSubmit(event) {
+      event.preventDefault();
+
+      // Find the text field via the React ref
+      const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+      Tasks.insert({
+        text,
+        createdAt: new Date(), // current time
+      });
+
+      // Clear form
+      ReactDOM.findDOMNode(this.refs.textInput).value = '';
+    }
 }
-
-
-
 export default withTracker(() => {
   return {
-    tasks: Tasks.find({}).fetch(),
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
   };
 })(App);
+<div className="container">
+  <header>
+    <h1>Todo List</h1>
+
+    <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+      <input
+          type="text"
+          ref="textInput"
+          placeholder="Type to add new tasks"
+      />
+    </form>
+  </header>
+
+  <ul>
